068ecb7bceefc954a320b6452265ea2142e5949f
Aftab Ahmed <w3aftab@gmail.com>
2026-07-25 22:40:19 +0600

    url changed

--- API.js
+++ API.js
@@ -1,24 +1,24 @@
 const API = {
-	products: "https://aftabwebdev.github.io/api/products/products.json",
-	products2: "https://aftabwebdev.github.io/json-api/products.json",
-	users: "https://aftabwebdev.github.io/json-api/users.json",
+  products: "https://w3aftab.github.io/api/products/products.json",
+  products2: "https://w3aftab.github.io/json-api/products.json",
+  users: "https://w3aftab.github.io/json-api/users.json",
 };
 
 async function getAPI(api, displayData) {
-	try {
-		const response = await fetch(api);
-		const data = await response.json();
-		displayData(data);
-	} catch (err) {
-		document.body.innerHTML = err.message;
-		console.log(err.message);
-	}
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    displayData(data);
+  } catch (err) {
+    document.body.innerHTML = err.message;
+    console.log(err.message);
+  }
 }
 
 export { API, getAPI };
 
 /** 
- fetch("https://aftabwebdev.github.io/api/products/products.json")
+ fetch("https://w3aftab.github.io/api/products/products.json")
 	 .then((res) => res.json())
 	 .then((data) => displayData(data))
 	 .catch((err) => err.message);
